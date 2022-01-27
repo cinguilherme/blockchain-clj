de086601248815ca54c18a49d223a84f868d0f32
cinguilherme <cinguilherme@gmail.com>
2022-01-27 20:39:51 -0300

    combining resolvers

--- src/blockchain/parser.clj
+++ src/blockchain/parser.clj
@@ -1,12 +1,15 @@
 (ns blockchain.parser
   (:require [com.wsscode.pathom.core :as p]
-            [com.wsscode.pathom.connect :as pc]))
+            [com.wsscode.pathom.connect :as pc :refer [defresolver]]))
 
 (def people-table
-  {1 {:person/id 1 :person/name "Sally" :person/age 32}
+  {1 {:person/id 1 :person/name "Sally" :person/age 32 :person/document 1}
    2 {:person/id 2 :person/name "Joe" :person/age 22}
    3 {:person/id 3 :person/name "Fred" :person/age 11}
    4 {:person/id 4 :person/name "Bobby" :person/age 55}})
+
+(def doc-trable
+  { 1 {:document/id 1 :document/title "TLOTR"}})
 
 (def list-table
   {:friends {:list/id     :friends
@@ -16,27 +19,32 @@
              :list/label  "Enemies"
              :list/people [4 3]}})
 
-(pc/defresolver person-resolver [env {:person/keys [id]}]
+(defresolver doc-resolver [env {:document/keys [id]}]
+  {::pc/input #{:document/id}
+   ::pc/output [:document/id :document/title]}
+  (get doc-trable id))
+
+(defresolver person-resolver [env {:person/keys [id]}]
   {::pc/input  #{:person/id}
    ::pc/output [:person/name :person/age]}
   (get people-table id))
 
-(pc/defresolver list-resolver [env {:list/keys [id]}]
+(defresolver list-resolver [env {:list/keys [id]}]
   {::pc/input  #{:list/id}
    ::pc/output [:list/label {:list/people [:person/id]}]}
   (when-let [list (get list-table id)]
     (assoc list
            :list/people (mapv (fn [id] {:person/id id}) (:list/people list)))))
 
-(pc/defresolver friends-resolver [env input]
+(defresolver friends-resolver [env input]
   {::pc/output [{:friends [:list/id]}]}
   {:friends {:list/id :friends}})
 
-(pc/defresolver enemies-resolver [env input]
+(defresolver enemies-resolver [env input]
   {::pc/output [{:enemies [:list/id]}]}
   {:enemies {:list/id :enemies}})
 
-(def resolvers [person-resolver list-resolver friends-resolver enemies-resolver])
+(def resolvers [person-resolver doc-resolver list-resolver friends-resolver enemies-resolver])
 
 (def pathom-parser
   (p/parser {::p/env     {::p/reader                 [p/map-reader
