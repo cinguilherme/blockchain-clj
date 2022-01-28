553887e7e6d226669c7ce927ea696e6fb900aeae
cinguilherme <cinguilherme@gmail.com>
2022-01-28 20:58:12 -0300

    yes, I got this even deeper

--- src/blockchain/parser.clj
+++ src/blockchain/parser.clj
@@ -3,15 +3,18 @@
             [com.wsscode.pathom.connect :as pc :refer [defresolver]]))
 
 (def people-table
-  {1 {:person/id 1 :person/name "Sally"
-      :person/age 32 :person/document 1
+  {1 {:person/id    1 :person/name "Sally"
+      :person/age   32 :person/document 1
       :person/files [1]}
    2 {:person/id 2 :person/name "Joe" :person/age 22}
    3 {:person/id 3 :person/name "Fred" :person/age 11}
    4 {:person/id 4 :person/name "Bobby" :person/age 55}})
 
 (def files
-  {1 {:file/id 1 :file/title "some" :file/content "thing"}})
+  {1 {:file/id 1 :file/title "some" :file/content "thing" :file/author 1}})
+
+(def author-table
+  {1 {:author/id 1 :author/name "gui" :author/age 35}})
 
 (def doc-trable
   {1 {:document/id 1 :document/title "TLOTR"}})
@@ -30,9 +33,16 @@
   (get doc-trable id))
 
 (defresolver file-resolver [env {:file/keys [id]}]
-  {::pc/input #{:file/id}
-   ::pc/output [:file/id :file/content :file/title]}
-  (get files id))
+  {::pc/input  #{:file/id}
+   ::pc/output [:file/id :file/content :file/title :file/author]}
+  (let [filex (get files id)
+        aid (:file/author filex)]
+    (assoc filex :file/author {:author/id aid})))
+
+(defresolver author-resolver [env {:author/keys [id]}]
+  {::pc/input  #{:author/id}
+   ::pc/output [:author/name :author/age :author/id]}
+  (get author-table id))
 
 (defresolver person-resolver [env {:person/keys [id]}]
   {::pc/input  #{:person/id}
@@ -55,7 +65,7 @@
   {::pc/output [{:enemies [:list/id]}]}
   {:enemies {:list/id :enemies}})
 
-(def resolvers [person-resolver file-resolver doc-resolver list-resolver friends-resolver enemies-resolver])
+(def resolvers [person-resolver author-resolver file-resolver doc-resolver list-resolver friends-resolver enemies-resolver])
 
 (def pathom-parser
   (p/parser {::p/env     {::p/reader                 [p/map-reader
