50f1c2b57da86fdb8c2b3140b95c41fbac8e9e4b
cinguilherme <cinguilherme@gmail.com>
2022-01-27 21:04:25 -0300

    with-redefs mocking internal collections for test

--- test/blockchain/parser_test.clj
+++ test/blockchain/parser_test.clj
@@ -1,9 +1,11 @@
 (ns blockchain.parser-test
   (:require [clojure.test :refer :all])
-  (:require [blockchain.parser :refer [api-handler api-parser doc-resolver]]))
+  (:require [blockchain.parser :refer [api-handler api-parser doc-resolver doc-trable people-table]]))
 
 (deftest api-handler-test
   (testing "how do I test a resolver?"
-    (is (= {[:person/id 1] {:person/name "Sally" :document/title "TLOTR"}}
-           (api-parser [{[:person/id 1] [:person/name
-                                         :document/title]}])))))
+    (with-redefs [doc-trable {1 {:document/id 1 :document/title "test"}}
+                  people-table {1 {:person/id 1 :document/id 1 :person/name "Sammy" :person/age 21}}]
+      (is (= {[:person/id 1] {:person/name "Sammy" :document/title "test"}}
+             (api-parser [{[:person/id 1] [:person/name
+                                           :document/title]}]))))))
